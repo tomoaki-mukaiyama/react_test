615aeb4d702bb3cc3a1a5afc01faff745d387e8b
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-10-16 17:43:32 +0900

    post to rails todo api

--- src/App.js
+++ src/App.js
@@ -3,28 +3,21 @@ import './App.css';
 import axios from 'axios'
 
 function App() {
-  const [images, setImages] = useState([]);
   const [text, setText] = useState('');
-  const [query, setQuery] = useState('golden retriever');
+  const [query, setQuery] = useState('');
 
    //第二引数の[query]が変更したら実行される
-  // useEffect(() => {
-  //   console.log('userEffectが走りました。')
-  //   fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  // console.log(data.results[0].urls)
-  //     setImages(data.results)
-  //   })
-  // }, [query])
+  useEffect(() => {
+    console.log('userEffectが走りました。')
+    axios.post(`http://localhost:3000/todos`, {task: query});
+  }, [query])
 
   //ボタンのsubmitで実行される setQueryが実行されて、useEffectが発火
   const onSubmit = (e) => {
-    axios.get(`http://localhost:3000/push`, {withCredentials: true});
     e.preventDefault(); //画面遷移を防ぐ
     setQuery(text);
     setText('');
-    console.log("onSubmitが呼ばれました");
+    console.log("onSubmitが呼ばれました。");
   }
   
   return(
@@ -40,18 +33,6 @@ function App() {
         </form>
       </div>
       <div className="container">
-        {
-          images.map(image => (
-            <div key={image.id} className="card">
-              <img src={image.urls.regular} className="card-img" alt="" />
-              <div className="card-contnet">
-                <div className="card-title">
-                  {image.alt_description}
-                </div>
-              </div>
-            </div>
-          ))
-        }
       </div>
     </div>
   )

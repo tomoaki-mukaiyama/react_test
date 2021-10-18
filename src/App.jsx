2ac0f2040ec50aac265a1a40059e2fe24d918c2c
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-10-18 17:00:57 +0900

    format

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Switch, Route, Link } from 'react-router-dom'
-import styled from 'styled-components'
-import TodoList from './TodoList'
-import AddTodo from './AddTodo'
-import TodoShow from './TodoShow'
-import './App.css';
+import { Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import TodoList from "./TodoList";
+import AddTodo from "./AddTodo";
+import TodoShow from "./TodoShow";
+import "./App.css";
 
 const Nabvar = styled.nav`
   border-bottom: solid 1px;
@@ -11,8 +11,7 @@ const Nabvar = styled.nav`
   display: flex;
   justify-content: space-around;
   align-items: center;
-`
-
+`;
 
 const NavItems = styled.ul`
   display: flex;
@@ -20,33 +19,28 @@ const NavItems = styled.ul`
   max-width: 40%;
   justify-content: space-around;
   list-style: none;
-`
+`;
 
 const NavItem = styled.li`
   font-size: 19px;
-`
+`;
 
 const Wrapper = styled.div`
   width: 1000px;
   max-width: 85%;
   margin: 20px auto;
-`
-
+`;
 
 function App() {
-  return(
+  return (
     <>
       <Nabvar>
         <NavItems>
           <NavItem>
-            <Link to="/">
-              POST NEW
-            </Link>
+            <Link to="/">POST NEW</Link>
           </NavItem>
           <NavItem>
-            <Link to="/todos/list">
-              LIST
-            </Link>
+            <Link to="/todos/list">LIST</Link>
           </NavItem>
         </NavItems>
       </Nabvar>
@@ -58,7 +52,7 @@ function App() {
         </Switch>
       </Wrapper>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ src/TodoList.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/todos")
+      .then((resp) => {
+        console.log(resp.data);
+        setTodos(resp.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="task-container">
+        <ul className="content-created-at">
+          {todos.map((todo) => {
+            return (
+              <Link to={"/todo/" + todo.id}>
+                <li>{todo.created_at}</li>
+              </Link>
+            );
+          })}
+        </ul>
+        <ul className="content-task">
+          {todos.map((todo) => {
+            return (
+              <Link to={"/todo/" + todo.id}>
+                <li>{todo.task}</li>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default TodoList;
